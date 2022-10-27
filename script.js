71d75a4e979531e8a2b87393b85521ebab583320
ahlem-20 <ahlemmh123@gmail.com>
2022-10-27 14:31:26 +0300

    Run without Debugging

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ console.log(text);
 function myFunction() {
   return text + ' welcome to my blog';
 }
-
-
 console.log(myFunction());
+
+function checkIsBingo(number) {
+  if (number > 5) {
+    return 'Bingo';
+  }else {
+    return 'oups';
+  }
+}
+console.log(checkIsBingo(3));
+console.log(checkIsBingo(5));
+console.log(checkIsBingo(7));
