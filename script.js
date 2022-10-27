85c1e62d0ebd36302adfd964e98c2266b1037db7
ahlem-20 <ahlemmh123@gmail.com>
2022-10-27 14:27:06 +0300

    Define JS function returning a string

---
+++ script.js
@@ -0,0 +1,9 @@
+const text = document.getElementById("paragraphe").textContent;
+console.log(text);
+
+function myFunction() {
+  return text + ' welcome to my blog';
+}
+
+
+console.log(myFunction());
